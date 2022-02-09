4afd347e785c668126aa2280fe128d508fb42973
vishal sam <vinishakr9@gmail.com>
2022-02-09 20:11:41 +0530

    Added wage calculation

--- Empdetails.java
+++ Empdetails.java
@@ -1,16 +1,21 @@
 package com.oops;
 
 public class Empdetails {
-	public static final int EMP_PRESENT=1;
-	public static final int EMP_ABSENT=0;
-
+	
 	public static void main(String[] args){
-
+	
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 2;
+		int empHrs = 0;
+		int empWage = 0;
+		
 		int empCheck=(int)Math.floor(Math.random()*10)%2;
 
-		if(empCheck == EMP_PRESENT)
-			System.out.println("Employee Is Present  ");
+		if(empCheck == IS_FULL_TIME)
+			empHrs = 8;
 		else
-			System.out.println("Employee Is Absent" );
+			empHrs = 0;
+		empWage = empHrs + EMP_RATE_PER_HOUR;
+		System.out.println("employee wage is: " + empWage);
 	}
 }
