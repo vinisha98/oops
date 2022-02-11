709d096f9ba073656abefc3a35597e5a67b38719
vishal sam <vinishakr9@gmail.com>
2022-02-11 18:52:16 +0530

    refactored code using switch case

--- Empdetails.java
+++ Empdetails.java
@@ -2,25 +2,26 @@ package com.oops;
 
 public class Empdetails {
 	
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
 	public static void main(String[] args){
 	
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		
 		int empCheck=(int)Math.floor(Math.random()*10)%2;
-		
-		if(empCheck == IS_PART_TIME) {
+		switch (empCheck) {
+		case  IS_PART_TIME:
 			empHrs = 4;
 			System.out.println("Employee is part timer");
-		}
-		else if(empCheck == IS_FULL_TIME) {
+			break;
+		case IS_FULL_TIME:
 			empHrs = 8;
 			System.out.println("Employee is full timer");
-		}
-		else {
+			break;
+		default:
 			empHrs = 0;
 			System.out.println("Employee is absent timer");
 		}
