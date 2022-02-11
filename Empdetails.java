9d7c55821ec848a8a29d0d918642d7de5bfda5d3
vishal sam <vinishakr9@gmail.com>
2022-02-11 18:42:47 +0530

    Added part timer details

--- Empdetails.java
+++ Empdetails.java
@@ -5,17 +5,26 @@ public class Empdetails {
 	public static void main(String[] args){
 	
 		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 2;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
 		
 		int empCheck=(int)Math.floor(Math.random()*10)%2;
-
-		if(empCheck == IS_FULL_TIME)
+		
+		if(empCheck == IS_PART_TIME) {
+			empHrs = 4;
+			System.out.println("Employee is part timer");
+		}
+		else if(empCheck == IS_FULL_TIME) {
 			empHrs = 8;
-		else
+			System.out.println("Employee is full timer");
+		}
+		else {
 			empHrs = 0;
+			System.out.println("Employee is absent timer");
+		}
 		empWage = empHrs + EMP_RATE_PER_HOUR;
 		System.out.println("employee wage is: " + empWage);
-	}
+		}
 }
