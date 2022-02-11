4fd2fc86dd1675ca71306b6658d932c141b7ffb9
vishal sam <vinishakr9@gmail.com>
2022-02-11 19:38:15 +0530

    Added multiple company data

--- Empdetails.java
+++ Empdetails.java
@@ -4,17 +4,14 @@ public class Empdetails {
 	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static int computeEmpWage(){
+	public static int computeEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth){
 	
 		int empHrs = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
 		
-		while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 		
 			totalWorkingDays++;
 			int empCheck=(int)Math.floor(Math.random()*10)%2;
@@ -33,12 +30,13 @@ public class Empdetails {
 			totalEmpHrs += empHrs;
 			System.out.println("day: " + totalWorkingDays + "Emp hrs: " +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR; 
+		int totalEmpWage = totalEmpHrs * empRatePerHour; 
 		System.out.println("Total Emp Wage: " + totalEmpWage);		
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("DMart",20,20,100);
+		computeEmpWage("DMart",40,50,200);
 	}
 }
