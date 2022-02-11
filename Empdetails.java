ab41222461b993fe6cb990a9f786f823d8cca03f
vishal sam <vinishakr9@gmail.com>
2022-02-11 19:03:15 +0530

    Added code for calculating per month wage

--- Empdetails.java
+++ Empdetails.java
@@ -5,27 +5,32 @@ public class Empdetails {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	
 	public static void main(String[] args){
 	
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		
+		for (int day=0; day < NUM_OF_WORKING_DAYS; day++) {
 		int empCheck=(int)Math.floor(Math.random()*10)%2;
+		
 		switch (empCheck) {
 		case  IS_PART_TIME:
 			empHrs = 4;
-			System.out.println("Employee is part timer");
 			break;
 		case IS_FULL_TIME:
 			empHrs = 8;
-			System.out.println("Employee is full timer");
 			break;
 		default:
 			empHrs = 0;
-			System.out.println("Employee is absent timer");
 		}
+		
 		empWage = empHrs + EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println("employee wage is: " + empWage);
 		}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+	}
 }
